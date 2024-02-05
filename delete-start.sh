f653f20f07dfd7f33e8b3772c6c31466c085e355
SantiTru <santiagotrujillonavarro@gmail.com>
2024-02-05 23:43:58 +0100

    Commit

--- delete-start.sh
+++ delete-start.sh
@@ -2,6 +2,9 @@
 STACK_NAME="TomcatPrueba"
 REGION=us-east-1
 CLI_PROFILE=default
+
+echo -e "\n=========== Eliminando pila ================="
+
 aws cloudformation delete-stack \
     --region us-east-1 \
     --profile default \
